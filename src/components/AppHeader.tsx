f8f33508641e42a574332932015a47951e9622d6
DominikReszczynski <dominikreszczynski10@gmail.com>
2023-09-18 17:05:29 +0200

    finish header

---
+++ src/components/AppHeader.tsx
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+import './AppHeader.css';
+import fischkappLogo from '../images/fischkappLogo.svg';
+import plusIcon from '../images/plusIcon.svg';
+
+export const AppHeader = () => {
+	const [count, setCount] = useState<number>(0);
+	return (
+		<header className='headerConteiner'>
+			<a href='' target='_blank'>
+				<img
+					src={fischkappLogo}
+					className='headerConteiner__logo'
+					alt='Fischkapp logo'
+				/>
+			</a>
+			<p className='headerConteiner__cardInfo'>{`Cards:${count}`}</p>
+			<button
+				className='headerConteiner__plusBtn'
+				onClick={() => setCount((count) => count + 1)}
+			>
+				<img src={plusIcon} alt='plus' />
+			</button>
+		</header>
+	);
+};
