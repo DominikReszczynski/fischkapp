eac1d3b39e97b790c33b9cd5989a56e414957b00
DominikReszczynski <dominikreszczynski10@gmail.com>
2023-09-19 18:02:18 +0200

    to check

--- src/components/AppCard.tsx
+++ src/components/AppCard.tsx
@@ -1,41 +1,133 @@
 import React, { useState } from 'react';
+// import style from './AppCardEdit.module.css';
+import binIcon from '../images/Kind=Delete.svg';
 import styles from './AppCard.module.css';
 import editIcon from '../images/Kind=Edit.svg';
 
-export const AppCard = () => {
-	const textArr: Array<string> = [
-		'What is Lorem Ipsum?',
-		'Lorem Ipsum is simply dummy text of the printing',
-		'and typesetting industry. Lorem Ipsum has been the industry',
-		'standard dummy text ever since the 1500s, when an unknown printer',
-		'took a galley of type and scrambled it to make a type',
-		'specimen book. It has survived not only five centuries, but also the leap',
-		'into electronic typesetting, remaining essentially unchanged.',
-	];
-	const [text, setText] = useState(textArr);
+interface Flashcard {
+	front: string;
+	back: string;
+}
 
-	const reverseText = (index: number) => {
-		const newTextArr = [...text];
-		newTextArr[index] = newTextArr[index].split('').reverse().join('');
-		setText(newTextArr);
+export const AppCard = () => {
+	const textArr: Flashcard[] = [
+		{ front: 'What is Lorem Ipsum?', back: 'fish' },
+		{ front: 'Lorem Ipsum is simply dummy text of the printing', back: 'fish' },
+		{
+			front: 'and typesetting industry. Lorem Ipsum has been the industry',
+			back: 'fish',
+		},
+		{
+			front:
+				'standard dummy text ever since the 1500s, when an unknown printer',
+			back: 'fish',
+		},
+		{
+			front: 'took a galley of type and scrambled it to make a type',
+			back: 'fish',
+		},
+		{
+			front:
+				'specimen book. It has survived not only five centuries, but also the leap',
+			back: 'fish',
+		},
+		{
+			front: 'into electronic typesetting, remaining essentially unchanged.',
+			back: 'fish',
+		},
+	];
+
+	const [cards, setCards] = useState<Flashcard[]>(textArr);
+	const [isEditing, setIsEditing] = useState<boolean[]>(
+		Array(textArr.length).fill(false)
+	);
+	const [editMode, setEditMode] = useState<boolean>(true);
+	const [secondPageVisable, setSecondPageVisable] = useState<boolean>(true);
+	const toggleCardSide = (index: number) => {
+		const newEditingState = [...isEditing];
+		newEditingState[index] = !newEditingState[index];
+		setIsEditing(newEditingState);
 	};
 
 	return (
-		<div>
-			{text.map((item, index) => (
-				<button
-					className={styles.cardBtn}
-					key={index}
-					onClick={() => reverseText(index)}
-				>
-					<div className={styles.card}>
-						<div className={styles.editConteiner}>
-							<img src={editIcon} alt='edit' />
+		<>
+			{editMode && (
+				<div>
+					{cards.map((card, index) => (
+						<button
+							className={styles.cardBtn}
+							key={index}
+							onClick={() => toggleCardSide(index)}
+						>
+							<div className={styles.card}>
+								<div className={styles.editConteiner}>
+									<button
+										onClick={() => {
+											console.log('jestem');
+											setEditMode(false);
+										}}
+									>
+										<img src={editIcon} alt='edit' />
+									</button>
+								</div>
+
+								<h4 className={styles.text}>
+									{isEditing[index] ? card.back : card.front}
+								</h4>
+							</div>
+						</button>
+					))}
+				</div>
+			)}
+			{!editMode && (
+				<>
+					{secondPageVisable && (
+						<div className={styles.cardConteinerNext}>
+							<textarea className={styles.inputText}></textarea>
+							<div>
+								<button
+									className={styles.btnCancel}
+									onClick={() => setEditMode(true)}
+								>
+									CANCEL
+								</button>
+								<button
+									className={styles.btnConfirm}
+									onClick={() => setSecondPageVisable(false)}
+								>
+									NEXT
+								</button>
+							</div>
 						</div>
-						<h4 className={styles.text}>{item}</h4>
-					</div>
-				</button>
-			))}
-		</div>
+					)}
+					{!secondPageVisable && (
+						<div className={styles.cardConteinerSave}>
+							<div className={styles.saveHeader}>
+								<button className={styles.binBtn}>
+									<img src={binIcon} alt='binIcon' />
+								</button>
+							</div>
+							<textarea className={styles.inputText}></textarea>
+							<div>
+								<button
+									className={styles.btnCancel}
+									onClick={() => setSecondPageVisable(true)}
+								>
+									BACK
+								</button>
+								<button
+									className={styles.btnConfirm}
+									onClick={() => {
+										setEditMode(true), setSecondPageVisable(true);
+									}}
+								>
+									SAVE
+								</button>
+							</div>
+						</div>
+					)}
+				</>
+			)}
+		</>
 	);
 };
