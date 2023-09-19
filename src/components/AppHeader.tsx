4f2dcd14fa360dd4360628ecbf134fca5ab94b9c
DominikReszczynski <dominikreszczynski10@gmail.com>
2023-09-19 08:08:56 +0200

    remove BEM

--- src/components/AppHeader.tsx
+++ src/components/AppHeader.tsx
@@ -1,22 +1,22 @@
 import React, { useState } from 'react';
-import './AppHeader.css';
+import styles from './AppHeader.module.css';
 import fischkappLogo from '../images/fischkappLogo.svg';
 import plusIcon from '../images/plusIcon.svg';
 
 export const AppHeader = () => {
 	const [count, setCount] = useState<number>(0);
 	return (
-		<header className='headerConteiner'>
+		<header className={styles.headerConteiner}>
 			<a href='' target='_blank'>
 				<img
 					src={fischkappLogo}
-					className='headerConteiner__logo'
+					className={styles.logo}
 					alt='Fischkapp logo'
 				/>
 			</a>
-			<p className='headerConteiner__cardInfo'>{`Cards :${count}`}</p>
+			<p className={styles.cardInfo}>{`Cards :${count}`}</p>
 			<button
-				className='headerConteiner__plusBtn'
+				className={styles.plusBtn}
 				// onClick={() => setCount((count) => count + 1)}
 			>
 				<img src={plusIcon} alt='plusIcon' />
