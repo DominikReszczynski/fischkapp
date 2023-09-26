2d3c96b82e878d68a00aaa3c8fc7ae8dc2eb37fd
Dominik <dominikreszczynski10@gmail.com>
2023-09-26 14:54:53 +0200

    fetch delete

--- src/components/AppCard/normal/normalCard.tsx
+++ src/components/AppCard/normal/normalCard.tsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import styles from './normalCard.module.css';
 import editIcon from '../../../images/Kind=Edit.svg';
-import binIcon from '../../../images/Kind=Delete.svg'
+import binIcon from '../../../images/Kind=Delete.svg';
+import { URL_FISCHKAPP, URL_TOKEN } from '../../../App';
 export const NormalCard = ({
   front,
   back,
@@ -10,43 +11,69 @@ export const NormalCard = ({
   changeSide,
   openEdit,
   cards,
+  setCards,
   index,
-  setCards
+  id,
+  onAddCard,
 }) => {
-  const [flip ,setFlip] = useState(false);
+  const [flip, setFlip] = useState(false);
   const deleteCard = () => {
-    let updatedCards = [...cards];
-    updatedCards.splice(index, 1);
-    setCards(updatedCards);
+    const requestOptions = {
+      method: 'DELETE',
+      headers: {
+        Authorization: URL_TOKEN,
+      },
+    };
+  
+    fetch(`${URL_FISCHKAPP}/${id}`, requestOptions)
+      .then((response) => {
+        if (!response.ok) {
+          console.log(response);
+          throw new Error('Wystąpił błąd podczas usuwania');
+        }
+        return response.json();
+      })
+      .then((data) => {
+        console.log('Odpowiedź serwera na usunięcie:', data);
+        onAddCard();
+      })
+      .catch((error) => {
+        console.error('Błąd:', error);
+      });
+      
   };
   useEffect(() => {
-    setTimeout(() => setFlip(false),400);
-  }, [flip])
+    setTimeout(() => setFlip(false), 400);
+  }, [flip]);
   return (
-    
-      <div className={`${styles.card} ${flip ? styles.flip : ''}`}>
-        <div className={styles.editConteiner}>
+    <div className={`${styles.card} ${flip ? styles.flip : ''}`}>
+      <div className={styles.editConteiner}>
         <button
-            onClick={() => {
-              deleteCard()
-            }}
-          >
-            <img src={binIcon} alt="bin" />
-          </button>
-          <button
-            onClick={() => {
-              openEdit();
-            }}
-          >
-            <img src={editIcon} alt="edit" />
-          </button>
-        </div>
+          onClick={() => {
+            deleteCard();
+            window.location.reload();
+          }}
+        >
+          <img src={binIcon} alt="bin" />
+        </button>
         <button
-          className={styles.cardBtn}
-          onClick={() => {(!isEdit ? (changeSide(), setFlip(true)) : null)}}>
-        <h4 className={styles.text}>{side ? cards[index]?.front : cards[index]?.back}</h4>
+          onClick={() => {
+            openEdit();
+          }}
+        >
+          <img src={editIcon} alt="edit" />
         </button>
       </div>
-    
+      <button
+        className={styles.cardBtn}
+        onClick={() => {
+          !isEdit ? (changeSide(), setFlip(true)) : null;
+        }}
+      >
+        <h4 className={styles.text}>
+          {side ? cards[index]?.front : cards[index]?.back}
+        </h4>
+      </button>
+    </div>
   );
 };
