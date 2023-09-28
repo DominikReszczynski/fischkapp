9aa3ae707d8182679bbb73cd927732e27fb44c7b
Dominik <dominikreszczynski10@gmail.com>
2023-09-28 13:56:58 +0200

    add test display

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,30 +1,43 @@
 import React from 'react';
-import { render, screen, fireEvent, waitFor } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
+import { rest } from 'msw';
+import { setupServer } from 'msw/node';
 import App from './App';
 
-global.fetch = jest.fn(() =>
-  Promise.resolve({
-    ok: true,
-    json: () => Promise.resolve([]), 
+// Tworzymy serwer HTTP Mock, aby obsłużyć żądanie GET do naszej usługi API (mockowanej)
+const server = setupServer(
+  rest.get('https://training.nerdbord.io/api/v1/fischkapp/flashcards', (req, res, ctx) => {
+    return res(
+      ctx.status(200),
+      ctx.json([
+        { _id: 'card1Id', front: 'Front 1', back: 'Back 1' },
+        { _id: 'card2Id', front: 'Front 2', back: 'Back 2' },
+      ])
+    );
   })
 );
 
-test('renders the App component', async () => {
-  render(<App />);
+beforeAll(() => server.listen());
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
 
-  await waitFor(() => expect(global.fetch).toHaveBeenCalledTimes(1));
+describe('App', () => {
+  it('should display flashcards in the list properly', async () => {
+    render(<App />);
 
-  const headerElement = screen.getByText(/Fischkapp Flashcards/i);
-  expect(headerElement).toBeInTheDocument();
+    // Oczekujemy, że komponent początkowo wyświetli informację "Brak fiszek, dodaj nowe"
+    const noCardsMessage = screen.getByText('Brak fiszek, dodaj nowe');
+    expect(noCardsMessage).toBeInTheDocument();
 
-  const addButtonElement = screen.getByText(/Add Card/i);
-  expect(addButtonElement).toBeInTheDocument();
+    // Czekamy na dane fiszek pobrane z serwera i sprawdzamy, czy są wyświetlane poprawnie
+    const front1 = await screen.findByText('Front 1');
+    const back1 = await screen.findByText('Back 1');
+    const front2 = await screen.findByText('Front 2');
+    const back2 = await screen.findByText('Back 2');
 
-  const noFlashcardsMessage = screen.getByText(/Brak fiszek, dodaj nowe/i);
-  expect(noFlashcardsMessage).toBeInTheDocument();
-
-  fireEvent.click(addButtonElement);
-
-  const cardAddForm = screen.getByText(/Front:/i);
-  expect(cardAddForm).toBeInTheDocument();
+    expect(front1).toBeInTheDocument();
+    expect(back1).toBeInTheDocument();
+    expect(front2).toBeInTheDocument();
+    expect(back2).toBeInTheDocument();
+  });
 });
